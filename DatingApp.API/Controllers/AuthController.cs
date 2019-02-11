df7c83032ddd001fdb379e07f05986f1abb2d6d5
WAUTERS William <william.wauters@ctr-consulting.be>
2019-02-11 16:57:05 +0100

    adding controller + user model

---
+++ DatingApp.API/Controllers/AuthController.cs
@@ -0,0 +1,10 @@
+namespace DatingApp.API.Controllers
+{
+     //GET http://localhost:5000/api/Auth
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController
+    {
+        
+    }
+}
---
+++ DatingApp.API/Data/IAuthRepository.cs
@@ -0,0 +1,9 @@
+namespace DatingApp.API.Data
+{
+    public interface IAuthRepository
+    {
+        Task<User> Register(User user, string password);
+        Task<User> Login(string usernamne, string password);
+        Task<bool> UserExists(string username);
+    }
+}
